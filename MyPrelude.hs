bb97b9386364704f5fa807c16557663b66942f44
Kiwamu Okabe <kiwamu@debian.or.jp>
2011-05-09 00:29:39 +0900

    add Maybe (Functor and Applicative). Not yet Monad

--- MyPrelude.hs
+++ MyPrelude.hs
@@ -1,19 +1,23 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 import Prelude (error)
 import GHC.Num ((+),(*),(-))
+import GHC.Show
 
 -- Basic
 id :: a -> a
 id a = a
 
--- Functor
+const :: a -> b -> a
+const x _ = x
+
+-- Functor ---------------------------------------------
 class Functor f where
   fmap, (<$>) :: (a -> b) -> f a -> f b
   (<$>) = fmap
   (<$) :: a -> f b -> f a
   (<$) a x = fmap (\_ -> a) x
 
--- Applicative
+-- Applicative -----------------------------------------
 class Functor f => Applicative f where
   return, pure :: a -> f a
   return = pure
@@ -21,15 +25,15 @@ class Functor f => Applicative f where
   (*>) :: f a -> f b -> f b
   (*>) x y = ((\_ -> id) <$> x) <*> y
   (<*) :: f a -> f b -> f a
-  (<*) x y = ((\i -> (\_ -> i)) <$> x) <*> y
+  (<*) x y = (const <$> x) <*> y
 
--- Monad
+-- Monad -----------------------------------------------
 class Applicative m => Monad m where
   (>>=) :: m a -> (a -> m b) -> m b
   -- (>>) :: m a -> m b -> m b
   -- fail :: String -> m a
 
--- [] (Functor,Monad,Eq,Ord,Read,Show)
+-- [] --------------------------------------------------
 -- data [] a = [] | a : [a] -- <= []自体の定義は構文糖でいいのかな？
 instance Functor [] where
   fmap f [] = []
@@ -42,12 +46,24 @@ instance Applicative [] where
       cat :: [a] -> [a] -> [a]
       cat [] y = y
       cat (x:xs) y = x : (xs `cat` y)
+-- xxx TODO: instance Monad [] where
 
--- IO
+-- Maybe -----------------------------------------------
+data Maybe a = Nothing | Just a
+             deriving (Show)
+instance Functor Maybe where
+  fmap _ Nothing = Nothing
+  fmap f (Just x) = Just (f x)
+instance Applicative Maybe where
+  pure a = Just a
+  (<*>) Nothing _ = Nothing
+  (<*>) _ Nothing = Nothing
+  (<*>) (Just x) y = x <$> y
 
--- Maybe
+-- IO --------------------------------------------------
 
--- State
+
+-- State -----------------------------------------------
 
 
 -- etc
